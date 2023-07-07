cbc2ce3c16077ff753ed53f9fe511f68fdf974d0
Vitalii Pahuta <91501209+TheStormix@users.noreply.github.com>
2023-07-07 17:18:09 +0300

    Update challange.py

--- challange.py
+++ challange.py
@@ -4,17 +4,15 @@ Given a list of distinct integers between 1 and 100 with length 99,
 how do you find out the number not present in the array as efficiently as possible (array is not sorted)
 '''
 import random
-
-def find_missing_number(numbers):
+def find_missing_number(numbers_list):
     result = 0
-    for num in numbers:
+    for num in numbers_list:
         result ^= num
     for num in range(1, 101):
         result ^= num
     return result
-
-exclude_number = random.randint(1, 100)
-numbers =[x for x in range(1, 101) if x != exclude_number]
-result = find_missing_number(numbers)
-print(numbers)
+numbers = list(range(1, 101))
+numbers_list = random.sample(numbers, 99)
+result = find_missing_number(numbers_list)
+print(numbers_list)
 print(result)
